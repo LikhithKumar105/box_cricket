14b494fe885565d6e3a51cc9cff523aa077573db
LikhithKumar105 <63649588+LikhithKumar105@users.noreply.github.com>
2026-06-19 18:45:20 +0530

    removed temp.jsx

--- src/temp.jsx
+++
@@ -1,5 +0,0 @@
-likhith105_db_user:Z98Nvr1QuaZ3YcWS
-
-"mongodb+srv://likhith105_db_user:Z98Nvr1QuaZ3YcWS@cluster0.b2dvtof.mongodb.net/"
-
-"mongodb+srv://likhith105_db_user:Z98Nvr1QuaZ3YcWS@cluster0.b2dvtof.mongodb.net/?appName=Cluster0"
